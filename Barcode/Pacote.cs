66f535bf762032a6e097095fc881f516350214ab
IsraelAlencar <israelalencarribeiro@gmail.com>
2022-04-04 15:14:12 -0400

    Programa Ciel ate o 6

---
+++ Barcode/Pacote.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barcode
+{
+    internal class Pacote
+    {
+        public string Codigo { get; set; }
+        public int NumOrigem { get; set; }
+        public int NumDestino { get; set; }
+        public int CodigoLoggi { get; set; }
+        public int CodigoVendedor { get; set; }
+        public int NumTipoProduto { get; set; }
+        public string RegiaoOrigem { get; set; }
+        public string RegiaoDestino { get; set; }
+        public string TipoProduto { get; set; }
+        public Boolean Valido { get; set; }
+
+        public Pacote() {
+
+        }
+
+        public Pacote(string codigo)
+        {
+            Codigo = codigo;
+
+            SeparaTrinca();
+        }
+
+        private void SeparaTrinca() {
+            int codigoOrigem = int.Parse(Codigo.Substring(0, 3));
+            int codigoDestino = int.Parse(Codigo.Substring(3, 3));
+            int codigoLoggi = int.Parse(Codigo.Substring(6, 3));
+            int codigoVendedor = int.Parse(Codigo.Substring(9, 3));
+            int codigoProduto = int.Parse(Codigo.Substring(12, 3));
+
+            NumOrigem = codigoOrigem;
+            NumDestino = codigoDestino;
+            CodigoLoggi = codigoLoggi;
+            CodigoVendedor = codigoVendedor;
+            NumTipoProduto = codigoProduto;
+
+            //Determinar regiao
+
+            RegiaoOrigem = VerificaRegiao(codigoOrigem);
+            RegiaoDestino = VerificaRegiao(codigoDestino);
+
+            //Determina o tipo do produto
+
+            TipoProduto = VerificaTipoProduto(codigoProduto);
+
+            //Verifica validade do pacote
+
+            if (RegiaoOrigem == null || RegiaoDestino == null || codigoVendedor == 367 || TipoProduto == null)
+            {
+                Valido = false;
+            }
+
+            else
+            {
+                Valido = true;
+            }
+        }
+
+        public string VerificaRegiao(int x) {
+
+            if (1 <= x && x <= 99)
+            {
+                return "Sudeste";
+            }
+
+            else if (100 <= x && x <= 199)
+            {
+                return "Sul";
+            }
+
+            else if (201 <= x && x <= 299)
+            {
+                return "Centro-oeste";
+            }
+
+            else if (300 <= x && x <= 399)
+            {
+                return "Nordeste";
+            }
+
+            else if (400 <= x && x <= 499)
+            {
+                return "Norte";
+            }
+
+            else {
+                return null;
+            }
+        }
+
+        public string VerificaTipoProduto(int x) {
+            if (x == 1)
+            {
+                return "Jóias";
+            }
+
+            else if (x == 111)
+            {
+                return "Livros";
+            }
+
+            else if (x == 333)
+            {
+                return "Eletrônicos";
+            }
+
+            else if (x == 555)
+            {
+                return "Bebidas";
+            }
+
+            else if (x == 888) {
+                return "Brinquedos";
+            }
+
+            else {
+                return null;
+            }
+        }
+    }
+}
---
+++ Barcode/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Barcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string codigo;
+            int contador = 0;
+
+            List<Pacote> list = new List<Pacote>();
+            List<Pacote> listSuldeste = new List<Pacote>();
+            List<Pacote> listSul = new List<Pacote>();
+            List<Pacote> listCentro = new List<Pacote>();
+            List<Pacote> listNordeste = new List<Pacote>();
+            List<Pacote> listNorte = new List<Pacote>();
+            List<Pacote> listValido = new List<Pacote>();
+            List<Pacote> listInvalido = new List<Pacote>();
+
+            while ((codigo = Console.ReadLine()) != null)
+            {
+
+                Pacote pacote = new Pacote(codigo);
+
+                list.Add(pacote);
+
+                if (pacote.RegiaoDestino == "Suldeste")
+                {
+                    listSuldeste.Add(pacote);
+                }
+
+                else if (pacote.RegiaoDestino == "Sul")
+                {
+                    listSul.Add(pacote);
+                }
+
+                else if (pacote.RegiaoDestino == "Centro-oeste")
+                {
+                    listCentro.Add(pacote);
+                }
+
+                else if (pacote.RegiaoDestino == "Nordeste")
+                {
+                    listNordeste.Add(pacote);
+                }
+
+                else
+                {
+                    listNorte.Add(pacote);
+                }
+
+                if (pacote.Valido == true)
+                {
+                    listValido.Add(pacote);
+                }
+
+                else
+                {
+                    listInvalido.Add(pacote);
+                }
+
+                contador += 1;
+            }
+
+            //A Loggi precisa:
+
+            //1.
+            Console.WriteLine();
+            Console.WriteLine("1.");
+
+            Console.WriteLine("Suldeste: " + listSuldeste.Count);
+            Console.WriteLine();
+
+            Console.WriteLine("Sul: " + listSul.Count);
+            Console.WriteLine();
+
+            Console.WriteLine("Centro-oeste: " + listCentro.Count);
+            Console.WriteLine();
+
+            Console.WriteLine("Nordeste: " + listNordeste.Count);
+            Console.WriteLine();
+
+            Console.WriteLine("Norte: " + listNorte.Count);
+
+            //2.
+            Console.WriteLine();
+            Console.WriteLine("2.");
+
+            Console.WriteLine("Pacotes validos:");
+
+            foreach (Pacote x in listValido)
+            {
+                Console.WriteLine(x.Codigo);
+            }
+
+            foreach (Pacote x in listInvalido)
+            {
+                Console.WriteLine(x.Codigo);
+            }
+
+            //3.
+            Console.WriteLine();
+            Console.WriteLine("3.");
+
+            Console.WriteLine("Pacotes postados na região Sul contendo brinquedos:");
+
+            foreach (Pacote x in listSul)
+            {
+                if (x.TipoProduto == "Brinquedos")
+                {
+                    Console.WriteLine(x.Codigo);
+                }
+            }
+
+            //4
+            Console.WriteLine();
+            Console.WriteLine("4.");
+
+            Console.WriteLine("Pacotes validos agrupados por região:");
+            Console.WriteLine();
+            Console.WriteLine("Suldeste:");
+
+            foreach (Pacote x in listSuldeste)
+            {
+                if (x.Valido == true)
+                {
+                    Console.WriteLine(x.Codigo);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sul:");
+
+            foreach (Pacote x in listSul)
+            {
+                if (x.Valido == true)
+                {
+                    Console.WriteLine(x.Codigo);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Centro-oeste:");
+
+            foreach (Pacote x in listCentro)
+            {
+                if (x.Valido == true)
+                {
+                    Console.WriteLine(x.Codigo);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Nordeste:");
+
+            foreach (Pacote x in listNordeste)
+            {
+                if (x.Valido == true)
+                {
+                    Console.WriteLine(x.Codigo);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Norte:");
+
+            foreach (Pacote x in listNorte)
+            {
+                if (x.Valido == true)
+                {
+                    Console.WriteLine(x.Codigo);
+                }
+            }
+
+            //5.
+            Console.WriteLine();
+            Console.WriteLine("5.");
+
+            List<Pacote> listValidoAux = listValido;
+
+            int count = 0;
+
+            Console.WriteLine(listValidoAux[0].CodigoVendedor + ": ");
+
+            while (listValidoAux.Count <= 0)
+            {
+                foreach (Pacote x in listValidoAux)
+                {
+                    if (listValidoAux[0].CodigoVendedor == x.CodigoVendedor)
+                    {
+                        count++;
+                        listValidoAux.Remove(x);
+                    }
+                }
+
+                Console.WriteLine(listValidoAux[0].CodigoVendedor + ": " + count);
+                count = 0;
+            }
+
+            //6.
+            Console.WriteLine();
+            Console.WriteLine("6.");
+
+            //Suldeste
+
+
+
+
+
+        }
+        
+    }
+}
